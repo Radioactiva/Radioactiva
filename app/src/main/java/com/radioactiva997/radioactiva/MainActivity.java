483e431c3ff1b92d90575ca68eece06ef5ea2f3b
Radioactiva <30667430+Radioactiva@users.noreply.github.com>
2017-08-02 14:09:11 -0600

    Creacion de clase

--- app/src/main/java/com/radioactiva997/radioactiva/MainActivity.java
+++ app/src/main/java/com/radioactiva997/radioactiva/MainActivity.java
@@ -2,12 +2,33 @@ package com.radioactiva997.radioactiva;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv_Opciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fun_CargarComponentes();
+    }
+
+    private void fun_CargarComponentes(){
+        lv_Opciones = (ListView)findViewById(R.id.lv_Opciones);
+        fun_CargarOpciones();
+    }
+
+    private void fun_CargarOpciones(){
+        lv_Opciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), String.valueOf(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
